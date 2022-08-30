614ca4f045a62007d4cf2683bbc239eb6ebb087a
pagoulid <pagoulid@gmail.com>
2022-08-30 14:51:54 +0300

    Testing for triangle

--- Triangle.js
+++ Triangle.js
@@ -8,6 +8,7 @@ import {createElement} from 'https://pagoulid.github.io/JS-Mathematical-Shapes/C
     let calcArea = (sides) =>{
         const sumSides = sides.reduce((prevVal,currVal)=>prevVal+currVal,0);
         let s = sumSides*0.5;
+        console.log(`s: ${s}`);
         const middleCalc = sides.reduce((prevVal,currVal)=>(s-prevVal)*(s-currVal),0);
         console.log(middleCalc)
     }
