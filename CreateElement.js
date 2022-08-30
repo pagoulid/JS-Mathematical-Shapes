bd9d1f37588c18a1656335ad76861c316012d542
pagoulid <pagoulid@gmail.com>
2022-08-30 13:25:07 +0300

    Testing js imports

---
+++ CreateElement.js
@@ -0,0 +1,10 @@
+export function squareElement(squareText,id){
+    let squareItem = document.getElementById(id);
+    let squarePelement = document.createElement('p');
+    let squareTextNode = document.createTextNode(squareText);
+
+    squarePelement.appendChild(squareTextNode);
+    squareItem.appendChild(squarePelement);
+    return callback('Done');
+
+}
--- Square.js
+++ Square.js
@@ -1,16 +1,43 @@
 /*_of_square : refers to a feature of the square shape itself*/ 
 /*square_of_ : refers to the mathematical operation x^2 , where x is number*/
-const SQ_SIDE = 9;
 
-let calcDiag_of_square = (side) =>{
 
-    let square_of_side = side**2;
+import {squareElement} from './CreateElement';
+
+    const SQ_SIDE = 9;
+    const SHAPE = 'square';
+
+    let calcDiag_of_square = (side) =>{
+
+        let square_of_side = side**2;
    
-    return  Math.sqrt(square_of_side+square_of_side);
+        return  Math.sqrt(square_of_side+square_of_side);
 
-}
+    }
 
-let side_of_square = SQ_SIDE;
-let diagonal_of_square = calcDiag_of_square(side_of_square);
 
-console.log(`Diagonal of square with side length ${side_of_square} is ${diagonal_of_square}`);
+
+
+    let side_of_square = SQ_SIDE;
+    let diagonal_of_square = calcDiag_of_square(side_of_square);
+
+    console.log(`Diagonal of square with side length ${side_of_square} is ${diagonal_of_square}`);
+    let resultText = `Diagonal of square with side length ${side_of_square} is ${diagonal_of_square}`;
+    console.log(resultText);
+    squareElement(resultText,SHAPE);
+
+
+
+
+
+
+    /*Visualize script results on HTML document*/ 
+/*function squareElement(squareText){
+    let squareItem = document.getElementById('square');
+    let squarePelement = document.createElement('p');
+    let squareTextNode = document.createTextNode(squareText);
+
+    squarePelement.appendChild(squareTextNode);
+    squareItem.appendChild(squarePelement);
+
+}*/
