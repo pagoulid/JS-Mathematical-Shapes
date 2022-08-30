2ef32efdee72da13e32560b0c56095fdcd21e1a2
pagoulid <pagoulid@gmail.com>
2022-08-30 16:05:46 +0300

    Add some styling...

--- CreateElement.js
+++ CreateElement.js
@@ -4,7 +4,13 @@ export function createElement(shapeText,id){
     let shapePelement = document.createElement('p');
     let shapeTextNode = document.createTextNode(shapeText);
 
+    let Pstyles ={
+        "color":"blue",
+        "font-size":"3rem"
+    }
     shapePelement.appendChild(shapeTextNode);
+
+    Object.assign(shapePelement.style,Pstyles);
     shapeItem.appendChild(shapePelement);
     
 
